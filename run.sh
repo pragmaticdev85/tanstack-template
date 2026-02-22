cbd4cf3f0c8d793abe641bf33c7ed9e6306276ba
Shanmukha Katuri <shanmukha.k@gmail.com>
2026-02-23 01:41:22 +0400

    Yes

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 cd "/home/shnmkhk/ws/tanstack-template"
+/usr/bin/git config --global --add safe.directory /home/shnmkhk/ws/tanstack-template
 /usr/bin/git pull origin main
 /home/shnmkhk/.nvm/versions/node/v25.2.1/bin/npm run yt
